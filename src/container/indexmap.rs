00a557ca0adb3895eedd3362780a486af44fbe9b
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2024-11-10 00:07:07 +0800

    (doc): Adding comments for indexmap methods
    (feat): IndexMap sort by values methods
    
    (refactor): Adding more tests
    
    * The overall crate function coverage hits 100%

--- src/container/indexmap.rs
+++ src/container/indexmap.rs
@@ -7,6 +7,49 @@ use std::{
     ops::{Deref, DerefMut},
 };
 
+/// A hash table where the iteration order of the key-value pairs
+/// is independent of the hash values of the keys.
+///
+/// The interface is closely compatible with the standard [`HashMap`](std::collections::HashMap),
+/// but also has additional features.
+///
+/// ## Order
+///
+/// See [`indexmap::IndexMap`] for more details.
+///
+/// ## Indices
+///
+/// See [`indexmap::IndexMap`] for more details.
+///
+/// ## Randomness
+///
+/// The [`IndexMap`] has a built-in random number generator ([`StdRng`]) for random operations.
+///
+/// It can be seeded with a [`u64`] value using the [`IndexMap::seed`] method.
+///
+/// ## Examples
+///
+/// ```rust
+/// use gausplat_loader::container::IndexMap;
+///
+/// let mut names = ["X", "X", "O"].into_iter().enumerate().collect::<IndexMap<_, _>>();
+///
+/// // Seed and sort values.
+/// names.seed(212).sort_unstable_values();
+///
+/// // Get values in random order.
+/// let names_target = vec!["X", "X", "X", "X", "O"];
+/// let names_output = names.random_values().take(5).copied().collect::<Vec<_>>();
+/// assert_eq!(names_output, names_target);
+///
+/// // Seed again.
+/// names.seed(212);
+///
+/// // Get key-value pairs in random order.
+/// let names_target = vec![(0, "X"), (1, "X"), (1, "X"), (1, "X"), (2, "O")];
+/// let names_output = names.random_iter().take(5).map(|(&k, &v)| (k, v)).collect::<Vec<_>>();
+/// assert_eq!(names_output, names_target);
+/// ```
 #[derive(Clone, Debug)]
 pub struct IndexMap<K, V, S = RandomState> {
     pub inner: IndexMapInner<K, V, S>,
@@ -23,6 +66,7 @@ impl<K, V, S> IndexMap<K, V, S> {
         self
     }
 
+    /// Get a key-value pair by random.
     pub fn get_random(&mut self) -> Option<(&K, &V)> {
         if self.inner.is_empty() {
             return None;
@@ -31,6 +75,7 @@ impl<K, V, S> IndexMap<K, V, S> {
             .get_index(self.rng.gen_range(0..self.inner.len()))
     }
 
+    /// Get a key-value pair by random.
     pub fn get_random_mut(&mut self) -> Option<(&K, &mut V)> {
         if self.inner.is_empty() {
             return None;
@@ -39,39 +84,74 @@ impl<K, V, S> IndexMap<K, V, S> {
             .get_index_mut(self.rng.gen_range(0..self.inner.len()))
     }
 
+    /// Get a key by random.
     #[inline]
     pub fn get_random_key(&mut self) -> Option<&K> {
         self.get_random().map(|(key, _)| key)
     }
 
+    /// Get a value by random.
     #[inline]
     pub fn get_random_value(&mut self) -> Option<&V> {
         self.get_random().map(|(_, value)| value)
     }
 
+    /// Get a value by random.
     #[inline]
     pub fn get_random_value_mut(&mut self) -> Option<&mut V> {
         self.get_random_mut().map(|(_, value)| value)
     }
 
+    /// Return an iterator over the key-value pairs of the map, in random order.
     pub fn random_iter(&mut self) -> impl Iterator<Item = (&K, &V)> {
         (&mut self.rng)
             .sample_iter(rand::distributions::Uniform::new(0, self.inner.len()))
             .filter_map(|index| self.inner.get_index(index))
     }
 
+    /// Return an iterator over the keys of the map, in random order.
     #[inline]
     pub fn random_keys(&mut self) -> impl Iterator<Item = &K> {
         self.random_iter().map(|(key, _)| key)
     }
 
+    /// Return an iterator over the values of the map, in random order.
     #[inline]
     pub fn random_values(&mut self) -> impl Iterator<Item = &V> {
         self.random_iter().map(|(_, value)| value)
     }
 }
 
+impl<K, V: Ord, S> IndexMap<K, V, S> {
+    /// Sort the map’s key-value pairs by the default ordering of the values,
+    /// while preserving the order of equal elements.
+    ///
+    /// Generally, [`sort_unstable_values`](IndexMap::sort_unstable_values)
+    /// is faster than [`sort_values`](IndexMap::sort_values).
+    ///
+    /// See [`sort_by`](IndexMapInner::sort_by) for details.
+    #[inline]
+    pub fn sort_values(&mut self) {
+        self.inner
+            .sort_by(|_, a_value, _, b_value| a_value.cmp(b_value));
+    }
+
+    /// Sort the map's key-value pairs by the default ordering of the keys, but
+    /// may not preserve the order of equal elements.
+    ///
+    /// Generally, [`sort_unstable_values`](IndexMap::sort_unstable_values)
+    /// is faster than [`sort_values`](IndexMap::sort_values).
+    ///
+    /// See [`sort_unstable_by`](IndexMapInner::sort_unstable_by) for details.
+    #[inline]
+    pub fn sort_unstable_values(&mut self) {
+        self.inner
+            .sort_unstable_by(|_, a_value, _, b_value| a_value.cmp(b_value));
+    }
+}
+
 impl<K, V> IndexMap<K, V> {
+    /// Create a new map. (Does not allocate.)
     #[inline]
     pub fn new() -> Self {
         Self {
@@ -80,6 +160,7 @@ impl<K, V> IndexMap<K, V> {
         }
     }
 
+    /// Create a new map with capacity (Does not allocate if `capacity` is zero.)
     #[inline]
     pub fn with_capacity(capacity: usize) -> Self {
         Self {
@@ -93,6 +174,7 @@ impl<K, V, S> Default for IndexMap<K, V, S>
 where
     S: Default,
 {
+    /// Return an empty [`IndexMap`] with a default RNG.
     #[inline]
     fn default() -> Self {
         Self {
@@ -136,6 +218,10 @@ where
     K: Hash + Eq,
     S: BuildHasher + Default,
 {
+    /// Create an [`IndexMap`] from the sequence of key-value pairs in the iterable.
+    ///
+    /// `from_iter` uses the same logic as [`extend`].
+    /// See [`extend`][IndexMapInner::extend] for more details.
     #[inline]
     fn from_iter<I: IntoIterator<Item = (K, V)>>(iterable: I) -> Self {
         Self {
@@ -148,7 +234,84 @@ where
 #[cfg(test)]
 mod tests {
     #[test]
-    fn seed_and_random_iteration() {
+    fn default_and_deref() {
+        use super::*;
+
+        let map = IndexMap::<u8, f32>::default();
+
+        let target = true;
+        let output = map.is_empty();
+        assert_eq!(output, target);
+
+        let target = &IndexMapInner::<u8, f32>::default();
+        let output = map.deref();
+        assert_eq!(output, target);
+    }
+
+    #[test]
+    fn from_iter() {
+        use super::*;
+
+        let map = IndexMap::<u8, f32>::from_iter([
+            (0, 0.1),
+            (3, 0.4),
+            (1, 0.2),
+            (4, 0.5),
+        ]);
+
+        let target = 4;
+        let output = map.len();
+        assert_eq!(output, target);
+    }
+
+    #[test]
+    fn get_random() {
+        use super::*;
+
+        let mut map = IndexMap::<u8, f32>::default();
+
+        let target = None;
+        let output = map.get_random_key();
+        assert_eq!(output, target);
+
+        let target = None;
+        let output = map.get_random_value().cloned();
+        assert_eq!(output, target);
+
+        let target = output;
+        let output = map.get_random_value_mut().cloned();
+        assert_eq!(output, target);
+
+        let mut map = IndexMap::<i16, f32>::with_capacity(1);
+        map.insert(-123, -4.5);
+
+        let target = Some(&-123);
+        let output = map.get_random_key();
+        assert_eq!(output, target);
+
+        let target = Some(&-4.5);
+        let output = map.get_random_value();
+        assert_eq!(output, target);
+
+        let target = Some(67.8);
+        map.insert(-123, 67.8);
+        let output = map.get_random_value_mut().cloned();
+        assert_eq!(output, target);
+    }
+
+    #[test]
+    fn new() {
+        use super::*;
+
+        let map = IndexMap::<u8, f32>::new();
+
+        let target = true;
+        let output = map.is_empty();
+        assert_eq!(output, target);
+    }
+
+    #[test]
+    fn seed_and_random_iter() {
         use super::*;
 
         let mut map: IndexMap<_, _> =
@@ -158,13 +321,13 @@ mod tests {
             .seed(0)
             .random_keys()
             .copied()
-            .take(32)
+            .take(256)
             .collect::<Vec<_>>();
         let values_2 = map
             .seed(0)
             .random_keys()
             .copied()
-            .take(32)
+            .take(256)
             .collect::<Vec<_>>();
         assert_eq!(values_1, values_2);
 
@@ -172,37 +335,44 @@ mod tests {
             .seed(0)
             .random_values()
             .copied()
-            .take(32)
+            .take(256)
             .collect::<Vec<_>>();
         let values_2 = map
             .seed(0)
             .random_values()
             .copied()
-            .take(32)
+            .take(256)
             .collect::<Vec<_>>();
         assert_eq!(values_1, values_2);
-
-        let _ = map.deref();
     }
 
     #[test]
-    fn no_panics() {
+    fn sort_values() {
         use super::*;
 
-        let mut map = IndexMap::<u8, f32>::default();
+        let mut map = IndexMap::<u8, u32>::from_iter(
+            [(0, 1), (2, 4), (1, 2), (3, 5)].iter().copied(),
+        );
 
-        assert!(map.get_random_key().is_none());
-        assert!(map.get_random_value().is_none());
-        assert!(map.get_random_value_mut().is_none());
+        map.sort_values();
 
-        let mut map = IndexMap::<u8, f32>::new();
-        assert!(map.get_random_key().is_none());
+        let target = vec![(0, 1), (1, 2), (2, 4), (3, 5)];
+        let output = map.iter().map(|(&k, &v)| (k, v)).collect::<Vec<_>>();
+        assert_eq!(output, target);
+    }
 
-        let mut map = IndexMap::<u8, f32>::with_capacity(1);
-        map.insert(0, 0.1);
+    #[test]
+    fn sort_unstable_values() {
+        use super::*;
 
-        assert_eq!(map.get_random_key(), Some(&0));
-        assert_eq!(map.get_random_value(), Some(&0.1));
-        assert_eq!(map.get_random_value_mut(), Some(&mut 0.1));
+        let mut map = IndexMap::<u8, u32>::from_iter(
+            [(0, 1), (2, 4), (1, 2), (3, 5)].iter().copied(),
+        );
+
+        map.sort_unstable_values();
+
+        let target = vec![(0, 1), (1, 2), (2, 4), (3, 5)];
+        let output = map.iter().map(|(&k, &v)| (k, v)).collect::<Vec<_>>();
+        assert_eq!(output, target);
     }
 }
